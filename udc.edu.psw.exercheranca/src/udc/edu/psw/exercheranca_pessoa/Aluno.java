3427a10c8ed6cacee07132f043636556d3382a24
adeli <adeli@DELIOs-pc>
2018-09-13 22:26:06 -0300

    terminando heranca

---
+++ udc.edu.psw.exercheranca/src/udc/edu/psw/exercheranca_pessoa/Aluno.java
@@ -0,0 +1,11 @@
+package udc.edu.psw.exercheranca_pessoa;
+
+public class Aluno extends Pessoa {
+	public String RA;
+	
+	public Aluno(String nome, String cpf,int idade) {
+		super ( nome, cpf, idade);
+	}
+		
+
+}
