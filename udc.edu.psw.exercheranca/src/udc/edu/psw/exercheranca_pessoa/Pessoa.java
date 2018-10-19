852fbed94e01d922152ef70da0dc4955f4d3bfc3
adeli <adeli@DELIOs-pc>
2018-10-19 00:49:32 -0300

    pessoa

---
+++ udc.edu.psw.exercheranca/src/udc/edu/psw/exercheranca_pessoa/Pessoa.java
@@ -0,0 +1,16 @@
+package udc.edu.psw.exercheranca_pessoa;
+
+public class Pessoa {
+	public String nome;
+	public int idade;
+	public String cpf;
+	//public int dada_aniversario
+	
+	public Pessoa(String nome, String cpf,int idade) {
+		this.nome=nome;
+		this.idade=idade;
+		this.cpf=cpf;
+	}
+	
+
+}
